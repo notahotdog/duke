d1a7d6d167f933022674e5dae596e34a4ba906ee
notahotdog <umarmoiz@gmail.com>
2019-08-22 23:26:53 +0800

    Level 2. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,25 +10,58 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can i do for you?");
         Boolean state = true;
-        List<String> ls1 = new ArrayList<String>();
+
+        List<Task> actionList = new ArrayList<>();
         while(state) {
+
             Scanner sc = new Scanner(System.in);
             String str = sc.nextLine();
+
+            boolean removeList = str.contains("done");
+
             if(str.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 state=false;
             }
+
+            else if(removeList){
+                System.out.println("Nice! I've marked this task as done:");
+                String arrStr[]  = str.split(" ", 2);
+
+                //this will change the icon to false
+                int num = Integer.parseInt(arrStr[1]) -1;
+                actionList.get(num).isDone = true;
+
+                String action = actionList.get(num).getDescription();
+                String check = actionList.get(num).getStatusIcon();
+                System.out.println("  [" + check +"]" + action);
+            }
+
+
             else if(str.equals("list")){
-                ListIterator<String> itr= ls1.listIterator();
-                while(itr.hasNext()) {
-                    System.out.println((itr.nextIndex()+1)+". " + itr.next());
+                System.out.println("Here are the tasks in your list:");
+
+                //Implementation using the task class
+                Iterator<Task> actionItr = actionList.iterator();
+                int ctr = 1;
+                while(actionItr.hasNext()){
+                    Task element = actionItr.next();
+                    String action = element.getDescription();
+                    String check = element.getStatusIcon();
+
+                    System.out.println(ctr +". [" + check +"]" + action);
+                    ctr++;
                 }
+
 
             }
             else {
-                ls1.add(str);
+
+                Task action = new Task(str);
+                actionList.add(action);
                 System.out.println("added: "+ str);
             }
         }
     }
 }
+
